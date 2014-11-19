2ca653748b54261c33cc02c7c2d68b138d97bdba
ur5y <vitor.medina@petrobras.com.br>
2014-11-19 15:23:54 -0200

    Groovy extension method working!!! \o/

--- src/main/groovy/sandbox/PayrollPage.groovy
+++ src/main/groovy/sandbox/PayrollPage.groovy
@@ -1,13 +1,10 @@
 package sandbox
 import com.vaadin.ui.AbsoluteLayout
-import com.vaadin.ui.MenuBar
 
 class PayrollPage extends AbsoluteLayout {
 
 
     PayrollPage() {
-        def bar = new MenuBar()
-        bar.addItem("Teste", null)
-        this.addComponent(bar)
+        this.addComponent(new Employee().asVaadinComponent())
     }
 }
