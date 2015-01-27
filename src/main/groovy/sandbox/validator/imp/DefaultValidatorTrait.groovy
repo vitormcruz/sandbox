99b3c63532aadc1b448b6d221fd70ba58a570edd
ur5y <vitor.medina@petrobras.com.br>
2015-01-27 16:33:00 -0200

    Removed unnecessary method.

--- src/main/groovy/sandbox/validator/imp/DefaultValidatorTrait.groovy
+++ src/main/groovy/sandbox/validator/imp/DefaultValidatorTrait.groovy
@@ -1,9 +1,7 @@
 package sandbox.validator.imp
-import org.junit.runner.Result
-import org.junit.runner.notification.RunListener
+
 import org.junit.runner.notification.RunNotifier
 import sandbox.validator.ParentValidatorRunner
-import sandbox.validator.ResultInterface
 import sandbox.validator.ValidatorTrait
 /**
  */
@@ -11,22 +9,6 @@ trait DefaultValidatorTrait implements ValidatorTrait{
 
     def private validatorRunner = new ValidatorRunner(this)
     def private runNotifier = new RunNotifier()
-
-    def ResultInterface validate(){
-        //TODO copied from JUnitCore, probably will change in function of specific listeners or notifiers from different layers (presentation, persistence etc) interested on validation result, but I don't know how yet.
-        Result result = new Result();
-        RunListener listener = result.createListener();
-        getNotifier().addFirstListener(listener);
-        try {
-            getNotifier().fireTestRunStarted(getValidatorRunner().getDescription());
-            getValidatorRunner().run(getNotifier());
-            getNotifier().fireTestRunFinished(result);
-        } finally {
-            getNotifier().removeListener(listener);
-        }
-
-        return result as ResultInterface;
-    }
 
     public ParentValidatorRunner getValidatorRunner() {
         return validatorRunner
